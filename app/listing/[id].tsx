3ba8508e22f52b986832ee7ac65ede46e1d803e4
josvillegas <josvillegas97@gmail.com>
2024-08-21 17:27:07 -0500

    add content final id view

--- app/listing/[id].tsx
+++ app/listing/[id].tsx
@@ -1,16 +1,237 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { router, Stack, useLocalSearchParams } from "expo-router";
+import { ListingType } from "@/types/listingType";
+import listingData from "@/data/destinations.json";
+import {
+  Feather,
+  FontAwesome,
+  FontAwesome5,
+  Ionicons,
+} from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
+
+const { width } = Dimensions.get("window");
+const IMG_HEIGHT = 300;
 
 const ListingsDetails = () => {
   const { id } = useLocalSearchParams();
+  const listing: ListingType = (listingData as ListingType[]).find(
+    (item) => item.id === id
+  );
   return (
-    <View>
-      <Text>ListingsDetails {id}</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          headerTransparent: true,
+          headerTitle: "",
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => router.back()}
+              style={{
+                backgroundColor: "rgba(255,255,255,0.5)",
+                borderRadius: 10,
+                padding: 4,
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: Colors.white,
+                  padding: 6,
+                  borderRadius: 10,
+                }}
+              >
+                <Feather name="arrow-left" size={20} />
+              </View>
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                backgroundColor: "rgba(255,255,255,0.5)",
+                borderRadius: 10,
+                padding: 4,
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: Colors.white,
+                  padding: 6,
+                  borderRadius: 10,
+                }}
+              >
+                <Feather name="bookmark" size={20} />
+              </View>
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <View style={styles.container}>
+        <ScrollView contentContainerStyle={{ paddingBottom: 150 }}>
+          <Image source={{ uri: listing.image }} style={styles.image} />
+          <View style={styles.contentWrapper}>
+            <Text style={styles.listingName}>{listing.name}</Text>
+            <View style={styles.listingLocationWrapper}>
+              <FontAwesome5
+                name="map-marker-alt"
+                size={18}
+                color={Colors.primaryColor}
+              />
+              <Text style={styles.listingLocationTxt}>{listing.location}</Text>
+            </View>
+
+            <View style={styles.highlightWrapper}>
+              {/* first highlight */}
+              <View style={{ flexDirection: "row" }}>
+                <View style={styles.highlightIcon}>
+                  <Ionicons name="time" size={18} color={Colors.primaryColor} />
+                </View>
+                <View>
+                  <Text style={styles.highlightTxt}>Duration</Text>
+                  <Text style={styles.highlightTxtValue}>
+                    {listing.duration} Days
+                  </Text>
+                </View>
+              </View>
+              {/* second highlight */}
+              <View style={{ flexDirection: "row" }}>
+                <View style={styles.highlightIcon}>
+                  <FontAwesome
+                    name="user"
+                    size={18}
+                    color={Colors.primaryColor}
+                  />
+                </View>
+                <View>
+                  <Text style={styles.highlightTxt}>Person</Text>
+                  <Text style={styles.highlightTxtValue}>
+                    {listing.duration}
+                  </Text>
+                </View>
+              </View>
+              {/* third highlight */}
+              <View style={{ flexDirection: "row" }}>
+                <View style={styles.highlightIcon}>
+                  <Ionicons name="star" size={18} color={Colors.primaryColor} />
+                </View>
+                <View>
+                  <Text style={styles.highlightTxt}>Rating</Text>
+                  <Text style={styles.highlightTxtValue}>
+                    {listing.rating} Days
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <Text style={styles.listingsDetails}>{listing.description}</Text>
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={[styles.footerBtn, styles.footerBookBtn]}
+        >
+          <Text style={styles.footerBtnTxt}>Book Now</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {}} style={styles.footerBtn}>
+          <Text style={styles.footerBtnTxt}>${listing.price}</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 
 export default ListingsDetails;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+  },
+  image: {
+    width: width,
+    height: IMG_HEIGHT,
+  },
+  contentWrapper: {
+    padding: 20,
+  },
+  listingName: {
+    fontSize: 24,
+    fontWeight: "500",
+    color: Colors.black,
+    letterSpacing: 0.5,
+  },
+  listingLocationWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 5,
+    marginBottom: 10,
+  },
+  listingLocationTxt: {
+    fontSize: 14,
+    marginLeft: 5,
+    color: Colors.black,
+  },
+  highlightWrapper: {
+    flexDirection: "row",
+    marginVertical: 20,
+    justifyContent: "space-between",
+  },
+  highlightIcon: {
+    backgroundColor: "#f4f4f4",
+    borderRadius: 18,
+    paddingHorizontal: 8,
+    paddingVertical: 5,
+    marginRight: 5,
+    alignItems: "center",
+  },
+  highlightTxt: {
+    fontSize: 12,
+    color: "#999",
+  },
+  highlightTxtValue: {
+    fontSize: 14,
+    fontWeight: "600",
+  },
+  listingsDetails: {
+    fontSize: 16,
+    color: Colors.black,
+    lineHeight: 24,
+    letterSpacing: 0.5,
+  },
+  footer: {
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 0,
+    padding: 20,
+    paddingBottom: 30,
+    width: width,
+  },
+  footerBtn: {
+    flex: 1,
+    backgroundColor: Colors.black,
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  footerBookBtn: {
+    flex: 2,
+    backgroundColor: Colors.primaryColor,
+    marginRight: 20,
+  },
+  footerBtnTxt: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: "600",
+    textTransform: "uppercase",
+  },
+});
